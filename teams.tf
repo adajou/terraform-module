2880635917c0b1d946af003ab478ba4aec78b3c9
Adam <116258864+adajou@users.noreply.github.com>
2023-10-12 15:33:47 +0200

    Update teams.tf

--- teams.tf
+++ teams.tf
@@ -1,18 +1,41 @@
-resource "github_team" "all" {
-    for_each                    = local.teams
-    name                        = each.value.name
-    description                 = each.value.description
-    privacy                     = each.value.privacy
-    create_default_maintainer   = true
+variable "github_teams" {
+  type = list(object({
+    name        = string
+    description = string
+    privacy     = string
+  }))
 }
 
-resource "github_team_membership" "members" {
-    #for_each                    = local.team_membership
-    for_each                    = { for team-membership in var.github_team_members : "var.github_team.name-${team-membership.username}" => team-membership }
-    team_id                     = each.value.team_id
-    username                    = each.value.username
-    role                        = each.value.role
-    depends_on = [
-        github_team.all
-    ]
+variable "github_team_members" {
+  type = map(list(object({
+    username = string
+    role     = string
+  })))
+}
+
+resource "github_team" "teams" {
+  for_each                  = { for team in var.github_teams : team.name => team }
+  name                      = each.value.name
+  description               = each.value.description
+  privacy                   = each.value.privacy
+  create_default_maintainer = true
+}
+
+resource "github_team_membership" "team-members" {
+  for_each = {
+    for item in flatten([
+      for team_name, team_members in var.github_team_members : [
+        for member in team_members : {
+          team_name = team_name
+          username  = member.username
+          role      = member.role
+        }
+      ]
+    ])
+    : "${item.team_name}-${item.username}" => item
+  }
+
+  team_id  = github_team.teams[each.value.team_name].id
+  username = each.value.username
+  role     = each.value.role
 }
